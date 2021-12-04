536b56fb48cbc2b5eab8e6285c54a1efc15023c9
Ferdinand Hubbard <ferdinandhubbard@gmail.com>
2021-12-04 21:36:54 +0000

    first commit

---
+++ converter.c
@@ -0,0 +1,18 @@
+
+
+void runUnitTests() {
+
+}
+
+int main(int n, char* args[n]) {
+    if (n == 2) {
+        //do stuff
+    }
+    else if (n == 1) {
+        runUnitTests();
+    }
+    else {
+        printf("usage ./converter filepath");
+    }
+    return 0;
+}
